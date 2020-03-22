cd588d064448d15b494fb97d9a6c4954d0b61dab
Yevgeny Levin <devlev1980@gmail.com>
2020-03-22 12:09:25 +0200

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  apiUrl = 'http://165.22.107.91:1234/v1';
   title = 'export-test';
+
+  constructor(private http: HttpClient) {
+  }
+
+  onDownload(projectId: string) {
+    this.http.get(`${this.apiUrl}/project/export_project/${projectId}`, {
+      responseType: 'arraybuffer',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/octet-stream'
+      })
+    }).subscribe(res => {
+      const blob = new Blob([res], {
+        type: 'application/zip'
+      });
+      const url = window.URL.createObjectURL(blob);
+      window.open(url);
+    });
+  }
+
+  ngOnInit(): void {
+  }
 }
